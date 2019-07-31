43f56b3b7656b576d6cff91ec40b2942e1ab5d66
Leandro Andreaci <leandro@focogroup.com>
2019-07-31 18:19:32 -0300

    add php7.0

--- php7.0/config/boostrap.sh
+++ php7.0/config/boostrap.sh
@@ -10,10 +10,18 @@ apk add curl
 apk add nginx
 apk add php7.0 php7.0-fpm 
 apk add php7.0-opcache 
-apk add php7.0-pdo php7.0-json php7.0-mbstring 
+apk add php7.0-json php7.0-mbstring 
+apk add php7.0-session php7.0-ctype 
 apk add php7.0-zlib 
 apk add php7.0-curl php7.0-mcrypt
 apk add php7.0-openssl
+apk add php7.0-mysqlnd
+apk add php7.0-mysqli
+apk add php7.0-pdo
+apk add php7.0-pdo_mysql
+apk add php7.0-tokenizer
+apk add php7.0-xml
+apk add php7.0-dom
 mkdir /run/nginx
 mkdir -p /var/www/html/
 
