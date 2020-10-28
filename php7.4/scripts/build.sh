50e14821d5b5a9d4c6d29693989cd57af6f2890c
Leandro Andreaci <leandroandreaci@hotmail.com>
2020-10-28 09:13:04 -0300

    add header to ngonx

--- php7.4/scripts/build.sh
+++ php7.4/scripts/build.sh
@@ -5,5 +5,5 @@ TAG=7.4
 docker build -t $IMAGE:$TAG .
 docker tag $IMAGE:$TAG $REPOSITORY/$IMAGE:$TAG
 docker tag $IMAGE:$TAG $REPOSITORY/$IMAGE:latest
-# docker push $REPOSITORY/$IMAGE:$TAG
-# docker push $REPOSITORY/$IMAGE:latest
+docker push $REPOSITORY/$IMAGE:$TAG
+docker push $REPOSITORY/$IMAGE:latest
