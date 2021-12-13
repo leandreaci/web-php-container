af850e1474d325b4be95b5881588f634d89f6d6c
Leandro Andreaci <leandroandreaci@hotmail.com>
2021-12-13 08:21:45 -0300

    fix: change to oci8-3.01 fixed in php8.0-oci8

--- php8.0-oci8/config/oci8.sh
+++ php8.0-oci8/config/oci8.sh
@@ -25,7 +25,7 @@ LD_LIBRARY_PATH=/opt/oracle/instantclient:$LD_LIBRARY_PATH
 
 #install oci8
 /usr/bin/pecl8 channel-update pecl.php.net
-sh -c "echo 'instantclient,/opt/oracle/instantclient' | /usr/bin/pecl8 install oci8"
+sh -c "echo 'instantclient,/opt/oracle/instantclient' | /usr/bin/pecl8 install oci8-3.0.1"
 
 #enable extension
 echo 'extension=oci8' > /etc/php8/conf.d/03_oci8.ini
