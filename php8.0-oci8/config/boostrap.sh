18bab0aa432518c1d6612f29676840dcd288ed95
Le <leandroandreaci@hotmail.com>
2022-03-09 12:26:04 -0300

    update php8.0-oci8

--- php8.0-oci8/config/boostrap.sh
+++ php8.0-oci8/config/boostrap.sh
@@ -34,14 +34,10 @@ apk add php$PHP_VERSION-fileinfo
 apk add php$PHP_VERSION-xmlwriter
 apk add php$PHP_VERSION-simplexml
 apk add php$PHP_VERSION-phar
+apk add composer
 
 ln -s /usr/bin/php8 /usr/bin/php
 
-#install composer 2.0
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
 
 mv composer.phar /usr/local/bin/composer
 
