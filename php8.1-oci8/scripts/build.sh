fedf1c3261fbf6006988301d04ba8f6139d2fc5f
Leandro <leandro.andreaci@sodexo.com>
2022-09-28 08:19:25 -0300

    fix: remove lastest ftom 8.1-oci8

--- php8.1-oci8/scripts/build.sh
+++ php8.1-oci8/scripts/build.sh
@@ -4,5 +4,4 @@ TAG=8.1-oci8
 
 docker build --no-cache -t $IMAGE:$TAG .
 docker tag $IMAGE:$TAG $REPOSITORY/$IMAGE:$TAG
-docker tag $IMAGE:$TAG $REPOSITORY/$IMAGE:latest
 docker push $REPOSITORY/$IMAGE:$TAG
