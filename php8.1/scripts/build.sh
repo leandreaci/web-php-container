f6b07e640e4333135baba437f5bad61053eaee29
João Pinheiro <joao.pinheiro.ext@sodexo.com>
2023-05-04 11:30:32 -0300

    add xdebug

--- php8.1/scripts/build.sh
+++ php8.1/scripts/build.sh
@@ -1,9 +1,14 @@
 REPOSITORY=leandreaci
 IMAGE=php
 TAG=8.1
+TAG_DEV=${TAG}_dev
 
-docker build -t $IMAGE:$TAG .
+docker build --no-cache -t $IMAGE:$TAG --target production .
 docker tag $IMAGE:$TAG $REPOSITORY/$IMAGE:$TAG
 docker tag $IMAGE:$TAG $REPOSITORY/$IMAGE:latest
 docker push $REPOSITORY/$IMAGE:$TAG
 docker push $REPOSITORY/$IMAGE:latest
+
+docker build --no-cache -t $IMAGE:$TAG_DEV --target development .
+docker tag $IMAGE:$TAG_DEV $REPOSITORY/$IMAGE:$TAG_DEV
+docker push $REPOSITORY/$IMAGE:$TAG_DEV
