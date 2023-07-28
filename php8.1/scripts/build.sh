2da2f111791a19b52727ff19b4a0d57b5d16441d
Leandro Andreaci <leandroandreaci@hotmail.com>
2023-07-28 17:55:00 -0300

    Revert "add xdebug"

--- php8.1/scripts/build.sh
+++ php8.1/scripts/build.sh
@@ -1,14 +1,9 @@
 REPOSITORY=leandreaci
 IMAGE=php
 TAG=8.1
-TAG_DEV=${TAG}-dev
 
-docker build --no-cache -t $IMAGE:$TAG --target production .
+docker build -t $IMAGE:$TAG .
 docker tag $IMAGE:$TAG $REPOSITORY/$IMAGE:$TAG
 docker tag $IMAGE:$TAG $REPOSITORY/$IMAGE:latest
 docker push $REPOSITORY/$IMAGE:$TAG
 docker push $REPOSITORY/$IMAGE:latest
-
-docker build --no-cache -t $IMAGE:$TAG_DEV --target development .
-docker tag $IMAGE:$TAG_DEV $REPOSITORY/$IMAGE:$TAG_DEV
-docker push $REPOSITORY/$IMAGE:$TAG_DEV
