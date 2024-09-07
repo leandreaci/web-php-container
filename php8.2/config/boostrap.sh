981a4a70ad4cf572964313288ec23e094e372515
Leandro Andreaci <leandroandreaci@gmail.com>
2024-09-07 09:18:14 -0300

    feat: add version 8.2

--- php8.2/config/boostrap.sh
+++ php8.2/config/boostrap.sh
@@ -31,11 +31,17 @@ apk add php$PHP_VERSION-fileinfo
 apk add php$PHP_VERSION-xmlwriter
 apk add php$PHP_VERSION-simplexml
 apk add php$PHP_VERSION-phar
-apk add composer
+apk add php$PHP_VERSION-gd
 
-ln -s /usr/bin/php81 /usr/bin/php
+rm /usr/bin/php
+ln -s /usr/bin/php$PHP_VERSION /usr/bin/php
 
-mv composer.phar /usr/local/bin/composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+mv composer.phar /usr/bin/composer
 
 #Nginx
 mkdir -p /var/www/html/
--- php8.2/config/start-php-fpm.sh
+++ php8.2/config/start-php-fpm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv sh
 set -e;
 
-/usr/sbin/php-fpm81 -R --nodaemonize
+/usr/sbin/php-fpm82 -R --nodaemonize
