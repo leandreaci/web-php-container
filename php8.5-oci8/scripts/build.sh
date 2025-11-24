79e7141ea27b1001f1f2ed20b0838dc3782a651c
Renato Balbino <renatodev001@gmail.com>
2025-11-24 10:13:08 -0300

    feat: add php 8.5 and php 8.5-oci8

---
+++ php8.5-oci8/scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+REPOSITORY=renatobalbino
+IMAGE=php
+TAG=8.5-oci8
+
+docker build --platform=linux/amd64 -t $IMAGE:$TAG .
+docker tag $IMAGE:$TAG $REPOSITORY/$IMAGE:$TAG
+docker tag $IMAGE:$TAG $REPOSITORY/$IMAGE:latest
+docker push $REPOSITORY/$IMAGE:$TAG
+docker push $REPOSITORY/$IMAGE:latest
